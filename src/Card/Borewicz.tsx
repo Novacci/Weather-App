99bc0fcdae342bb9b4681083c914fc982377236c
Wojciech Nowak <wojtekn00@gmail.com>
2023-01-16 01:23:16 +0100

    added test component "Borewicz"

---
+++ src/Card/Borewicz.tsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const Borewicz = () => {
+  const borciu = [1, 2, 3];
+
+  const borciu2 = borciu.map((x) => x * 2);
+
+  console.log(borciu2);
+
+  return <></>;
+};
+
+export default Borewicz;
